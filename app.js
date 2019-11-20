c852f6f00187bd74c247a56b60697f49c9c4e555
Oskar <o.mizgier@gmail.com>
2019-11-20 11:26:11 +0000

    app.listen

--- app.js
+++ app.js
@@ -185,6 +185,6 @@ function isLoggedIn(req, res, next){
 }
 
 //Tel express to listen for requests - start server
-app.listen(3000, function() { 
-  console.log('Server listening on port 3000'); 
-});
+.listen(process.env.PORT || 5000, function (){
+	console.log('App is running!')
+})
